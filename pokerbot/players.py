f28be32f030f4e9c733dff3e69939e16f7d6fcbf
Sam Ireland <sam.ireland.uk@gmail.com>
2017-10-06 23:56:53 +0100

    Start Player class

---
+++ pokerbot/players.py
@@ -0,0 +1,18 @@
+"""This module contains the Player class."""
+
+class Player:
+    """Represents a player in a game of Poker.
+
+    :param str name: The player's name.
+    :param int money: The money the player starts with.
+    :raises TypeError: if name is not a string.
+    :raises TypeError: if money is not an integer."""
+
+    def __init__(self, name, money=0):
+        if not isinstance(name, str):
+            raise TypeError("name {} is not str".format(name))
+        if not isinstance(money, int):
+            raise TypeError("money {} is not int".format(money))
+        self._name = name
+        self._money = money
+        self._cards = []
---
+++ tests/unit/test_players.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+from pokerbot.players import Player
+
+class GameCreationTests(TestCase):
+
+    def test_can_create_player(self):
+        player = Player("Jo")
+        self.assertEqual(player._name, "Jo")
+        self.assertEqual(player._money, 0)
+        self.assertEqual(player._cards, [])
+
+
+    def test_player_name_must_be_str(self):
+        with self.assertRaises(TypeError):
+            Player(100)
+
+
+    def test_can_create_player_with_money(self):
+        player = Player("Jo", money=200)
+        self.assertEqual(player._name, "Jo")
+        self.assertEqual(player._money, 200)
+        self.assertEqual(player._cards, [])
+
+
+    def test_player_money_must_be_int(self):
+        with self.assertRaises(TypeError):
+            Player("Jo", money="grand")
