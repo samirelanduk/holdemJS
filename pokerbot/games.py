a3c9e2f618b74018ca4b1d8b064260dd8f4e96d7
Sam Ireland <sam.ireland.uk@gmail.com>
2017-10-07 00:11:05 +0100

    Add Player addition

--- pokerbot/games.py
+++ pokerbot/games.py
@@ -1,5 +1,7 @@
 """This module contains the Game class."""
 
+from .players import Player
+
 class Game:
     """Represents a game of Poker, and tracks its state."""
 
@@ -9,3 +11,14 @@ class Game:
 
     def __repr__(self):
         return "<Game ({} players)>".format(len(self._players))
+
+
+    def add_player(self, player):
+        """Adds a :py:class:`.Player` to the game.
+
+        :param Player player: the player to add.
+        :raises TypeError: if the player is not a :py:class:`.Player`."""
+        
+        if not isinstance(player, Player):
+            raise TypeError("{} is not a Player".format(player))
+        self._players.append(player)
--- tests/unit/test_games.py
+++ tests/unit/test_games.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
+from unittest.mock import Mock, patch
 from pokerbot.games import Game
+from pokerbot.players import Player
 
 class GameCreationTests(TestCase):
 
@@ -15,3 +17,19 @@ class GameReprTests(TestCase):
         game = Game()
         game._players = [1, 5, 2, 5]
         self.assertEqual(str(game), "<Game (4 players)>")
+
+
+
+class GamePlayerAdditionTests(TestCase):
+
+    def test_can_add_player(self):
+        game = Game()
+        player = Mock(Player)
+        game.add_player(player)
+        self.assertEqual(game._players, [player])
+
+
+    def test_can_only_add_players(self):
+        game = Game()
+        with self.assertRaises(TypeError):
+            game.add_player("player")
