0df3a38961f03c953df87e7bf42de4c7a84b59fb
Sam Ireland <sam.ireland.uk@gmail.com>
2017-10-07 00:02:10 +0100

    Add player name

--- tests/unit/test_players.py
+++ tests/unit/test_players.py
@@ -33,3 +33,23 @@ class PlayerReprTests(TestCase):
     def test_player_repr(self):
         player = Player("Jo", money=200)
         self.assertEqual(str(player), "<Player 'Jo' (200)>")
+
+
+
+class VariableNameTests(TestCase):
+
+    def test_can_get_player_name(self):
+        player = Player("Jo")
+        self.assertIs(player._name, player.name())
+
+
+    def test_can_update_name(self):
+        player = Player("Jo")
+        player.name("new name")
+        self.assertEqual(player._name, "new name")
+
+
+    def test_new_name_must_be_str(self):
+        player = Player("Jo")
+        with self.assertRaises(TypeError):
+            player.name(100)
