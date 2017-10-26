27f478c3e7153feb85fd16cb60b3e024e0e7934a
Sam Ireland <sam.ireland.uk@gmail.com>
2017-10-26 19:00:38 +0100

    Start deck

--- holdem.js
+++ holdem.js
@@ -1,2 +1,10 @@
 var HOLDEM = true;
 var VERSION = 0.1;
+
+var deck = [];
+for (var rank = 1; rank <= 13; rank++) {
+  for (var i = 0; i <= 3; i++) {
+    deck.push([rank, ["H", "D", "S", "C"][i]]);
+  }
+}
+console.log(deck);
