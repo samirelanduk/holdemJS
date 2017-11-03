478fb103cafb3a71baf71c9b0b9c9cc2ed78d1e4
Sam Ireland <sam.ireland.uk@gmail.com>
2017-11-03 21:36:29 +0000

    Add blinds

--- holdem.js
+++ holdem.js
@@ -34,10 +34,27 @@ function Deck() {
 function Player(name) {
     this.name = name;
     this.cards = [];
+    this.game = null;
+
+    this.joinGame = function(game) {
+        game.players.push(this);
+        this.game = game;
+    }
+
+    this.nextPlayer = function() {
+        playerIndex = this.game.players.findIndex(x => x == this);
+        if (playerIndex == this.game.players.length - 1) {
+            return this.game.players[0];
+        } else {
+            return this.game.players[playerIndex + 1];
+        }
+    }
 }
 
-function Game(deck) {
+function Game(deck, sb, bb) {
     this.deck = deck;
+    this.sb = sb;
+    this.bb = bb;
     this.players = [];
     this.dealer = null;
 
@@ -45,4 +62,13 @@ function Game(deck) {
         var i = Math.floor(Math.random() * (this.players.length));
         this.dealer = this.players[i];
     }
+
+    this.smallBlind = function() {
+        return this.dealer.nextPlayer();
+    }
+
+    this.bigBlind = function() {
+        return this.smallBlind().nextPlayer();
+    }
+    
 }
